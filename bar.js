bb0caff5c924492289492c0633ba9e6c5463802e
Richardjackson-web <richardjackson556@gmail>
2022-11-09 14:28:31 +0100

    I got rid of some files

--- bar.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("mySidebar").style.width = "0";
-function openNav() {
-    document.getElementById("mySidebar").style.width = "50%";
-    document.getElementById("main").style.display = "block"
-  }
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
