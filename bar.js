59781f7bce54dd35b4013a6ede2b3a4a9ea7062f
Richardjackson-web <richardjackson556@gmail>
2022-11-09 12:18:40 +0100

    I created the landing page UI for a betting site

---
+++ bar.js
@@ -0,0 +1,9 @@
+document.getElementById("mySidebar").style.width = "0";
+function openNav() {
+    document.getElementById("mySidebar").style.width = "50%";
+    document.getElementById("main").style.display = "block"
+  }
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
